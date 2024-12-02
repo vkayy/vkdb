369272bd38e9ec617145fa3c3ea79e2171c8759e
vkayy <vkrice2475@gmail.com>
2024-12-02 04:21:51 +0000

    test: update lru cache test coverage and apply aaa and gwt test patterns

--- test/storage/lru_cache_test.cpp
+++ test/storage/lru_cache_test.cpp
@@ -3,38 +3,93 @@
 
 class LRUCacheTest : public ::testing::Test {
 protected:
-  static constexpr LRUCacheCapacity CACHE_CAPACITY{3};
+  using Cache = LRUCache<int, int>;
+  
+  static constexpr CacheCapacity CACHE_CAPACITY{3};
 
   void SetUp() override {
-    cache = std::make_unique<LRUCache<int, int>>(CACHE_CAPACITY);
+    cache_ = std::make_unique<Cache>(CACHE_CAPACITY);
   }
 
-  std::unique_ptr<LRUCache<int, int>> cache;
+  std::unique_ptr<Cache> cache_;
 };
 
-TEST_F(LRUCacheTest, ValuesPutInCacheCanBeRetrieved) {
-  cache->put(1, 1);
-  cache->put(2, 2);
-  EXPECT_EQ(cache->get(1), 1);
-  EXPECT_EQ(cache->get(2), 2);
+TEST_F(LRUCacheTest, CanObtainValueWhenKeyExists) {
+  cache_->put(1, 1);
+  cache_->put(2, 2);
+
+  auto get1{cache_->get(1)};
+  auto get2{cache_->get(2)};
+
+  EXPECT_EQ(get1, 1);
+  EXPECT_EQ(get2, 2);
 }
 
-TEST_F(LRUCacheTest, ValuesPutInCacheCanBeUpdated) {
-  cache->put(1, 1);
-  cache->put(2, 2);
-  cache->put(1, 10);
-  EXPECT_EQ(cache->get(1), 10);
-  EXPECT_EQ(cache->get(2), 2);
+TEST_F(LRUCacheTest, CanUpdateValueWhenKeyExists) {
+  cache_->put(1, 1);
+  cache_->put(1, 2);
+
+  auto get1{cache_->get(1)};
+
+  EXPECT_EQ(get1, 2);
 }
 
-TEST_F(LRUCacheTest, LeastRecentlyUsedValueIsEvictedWhenCapacityIsReached) {
-  cache->put(1, 1);
-  cache->put(2, 2);
-  cache->put(3, 3);
-  EXPECT_EQ(cache->get(1), 1);
-  cache->put(4, 4);
-  EXPECT_EQ(cache->get(1), 1);
-  EXPECT_EQ(cache->get(2), std::nullopt);
-  EXPECT_EQ(cache->get(3), 3);
-  EXPECT_EQ(cache->get(4), 4);
+TEST_F(LRUCacheTest, EvictsLeastRecentlyUsedWhenFull) {
+  cache_->put(1, 1);
+  cache_->put(2, 2);
+  cache_->put(3, 3);
+  cache_->put(4, 4);
+
+  auto contains1{cache_->contains(1)};
+  auto contains2{cache_->contains(2)};
+  auto contains3{cache_->contains(3)};
+  auto contains4{cache_->contains(4)};
+
+  EXPECT_FALSE(contains1);
+  EXPECT_TRUE(contains2);
+  EXPECT_TRUE(contains3);
+  EXPECT_TRUE(contains4);
+}
+
+TEST_F(LRUCacheTest, DoesNotModifyWhenCheckingKeyExists) {
+  cache_->put(1, 1);
+  cache_->put(2, 2);
+  cache_->put(3, 3);
+
+  auto contains4{cache_->contains(4)};
+  auto contains1{cache_->contains(1)};
+  auto contains2{cache_->contains(2)};
+  auto contains3{cache_->contains(3)};
+
+  EXPECT_FALSE(contains4);
+  EXPECT_TRUE(contains1);
+  EXPECT_TRUE(contains2);
+  EXPECT_TRUE(contains3);
+}
+
+TEST_F(LRUCacheTest, ReturnsCacheCapacityWhenCapacityChecked) {
+  auto capacity{cache_->capacity()};
+
+  EXPECT_EQ(capacity, CACHE_CAPACITY);
+}
+
+TEST_F(LRUCacheTest, ReturnsNumberOfElementsWhenSizeChecked) {
+  cache_->put(1, 1);
+  cache_->put(2, 2);
+  
+  auto size{cache_->size()};
+
+  EXPECT_EQ(size, 2);
+}
+
+TEST_F(LRUCacheTest, ContentsEmptiedWhenCacheCleared) {
+  cache_->put(1, 1);
+  cache_->put(2, 2);
+  cache_->clear();
+
+  auto get1{cache_->get(1)};
+  auto get2{cache_->get(2)};
+
+  EXPECT_EQ(get1, std::nullopt);
+  EXPECT_EQ(get2, std::nullopt);
 }
