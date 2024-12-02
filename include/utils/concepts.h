9ecd55c72086047cb37f50c80b42d82d7391e0cc
vkayy <vkrice2475@gmail.com>
2024-12-02 22:42:27 +0000

    feat: add arithmetic concept

--- include/utils/concepts.h
+++ include/utils/concepts.h
@@ -7,4 +7,7 @@ template <typename T, typename U>
 concept SameNoCVRefQuals =
   std::is_same_v<std::remove_cvref_t<T>, std::remove_cvref_t<U>>;
 
+template <typename T>
+concept Arithmetic = std::is_arithmetic_v<T>;
+
 #endif // UTILS_CONCEPTS_H
