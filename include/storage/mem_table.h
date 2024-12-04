7bfe1a9c5a2220c38c66b0d264762ce682eb8bd6
vkayy <vkrice2475@gmail.com>
2024-12-04 02:10:22 +0000

    feat: add conversion to and from string representation for memtable

--- include/storage/mem_table.h
+++ include/storage/mem_table.h
@@ -9,10 +9,9 @@ template <Arithmetic TValue>
 class MemTable {
 public:
   using key_type = TimeSeriesKey;
-  using mapped_type = TValue;
+  using mapped_type = std::optional<const TValue>;
+  using value_type = std::pair<const key_type, mapped_type>;
   using size_type = uint64_t;
-  using const_mapped = const mapped_type;
-  using opt_const_mapped = std::optional<const_mapped>;
 
   MemTable() noexcept = default;
   
@@ -29,7 +28,7 @@ public:
     update_ranges(key);
   }
 
-  [[nodiscard]] opt_const_mapped get(const key_type& key) const {
+  [[nodiscard]] mapped_type get(const key_type& key) const {
     if (!in_range(key)) {
       return std::nullopt;
     }
@@ -40,10 +39,68 @@ public:
     return it->second;
   }
 
+  [[nodiscard]] const std::vector<value_type> table() const noexcept {
+    std::vector<value_type> result;
+    for (const auto& [key, value] : table_) {
+      result.emplace_back(key, value);
+    }
+    return result;
+  }
+
+  [[nodiscard]] size_type size() const noexcept {
+    return table_.size();
+  }
+
+  [[nodiscard]] std::string toString() const noexcept {
+    std::stringstream ss;
+    ss << size() << "\n";
+    for (const auto& [key, value] : table()) {
+      ss << key.toString() << " | ";
+      if (value.has_value()) {
+        ss << value.value();
+      } else {
+        ss << "null";
+      }
+      ss << "\n";
+    }
+    return ss.str();
+  }
+
+  static void fromString(const std::string& str, MemTable& table) {
+    std::istringstream is(str);
+    
+    size_t size;
+    is >> size;
+    
+    std::string line;
+    std::getline(is, line);
+    
+    for (size_t i = 0; i < size; ++i) {
+      std::getline(is, line);
+      auto separator_pos = line.find(" | ");
+      
+      auto key_str = line.substr(0, separator_pos);
+      auto value_str = line.substr(separator_pos + 3);
+      
+      key_type key = key_type::fromString(key_str);
+      std::optional<TValue> value;
+      
+      if (value_str == "null") {
+        value = std::nullopt;
+      } else {
+        TValue temp;
+        std::istringstream{value_str} >> temp;
+        value = temp;
+      }
+      
+      table.put(key, value);
+    }
+  }
+
 private:
   using TimeRange = DataRange<Timestamp>;
   using KeyRange = DataRange<key_type>;
-  using Table = std::multimap<key_type, opt_const_mapped>;
+  using Table = std::multimap<const key_type, mapped_type>;
 
   [[nodiscard]] bool in_range(const key_type& key) const noexcept {
     return time_range_.in_range(key.timestamp()) && key_range_.in_range(key);
--- test/storage/mem_table_test.cpp
+++ test/storage/mem_table_test.cpp
@@ -1,13 +1,17 @@
 #include "gtest/gtest.h"
 #include "storage/mem_table.h"
 
+#include <sstream>
+
 class MemTableTest : public ::testing::Test {
 protected:
+  using Table = MemTable<int>;
+
   void SetUp() override {
-    table_ = std::make_unique<MemTable<int>>();
+    table_ = std::make_unique<Table>();
   }
 
-  std::unique_ptr<MemTable<int>> table_;
+  std::unique_ptr<Table> table_;
 };
 
 TEST_F(MemTableTest, CanPutAndGetValuesOfKeys) {
@@ -58,6 +62,51 @@ TEST_F(MemTableTest, CanPutAndGetValuesOfKeysWithMultipleTags) {
   auto value1{table_->get(key1)};
   auto value2{table_->get(key2)};
   auto value3{table_->get(key3)};
+
+  EXPECT_EQ(value1, 1);
+  EXPECT_EQ(value2, 2);
+  EXPECT_EQ(value3, 3);
+}
+
+TEST_F(MemTableTest, CanConvertToStringRepresentationWithTags) {
+  TimeSeriesKey key1{1, "metric1", {{"tag1", "value1"}}};
+  TimeSeriesKey key2{2, "metric2", {{"tag2", "value2"}}};
+  TimeSeriesKey key3{3, "metric3", {{"tag3", "value3"}}};
+
+  table_->put(key1, 1);
+  table_->put(key2, 2);
+  table_->put(key3, 3);
+
+  auto str{table_->toString()};
+
+  auto expected_str{
+    "3\n"
+    "{00000000000000000001}{metric1}{tag1:value1} | 1\n"
+    "{00000000000000000002}{metric2}{tag2:value2} | 2\n"
+    "{00000000000000000003}{metric3}{tag3:value3} | 3\n"
+  };
+
+  EXPECT_EQ(str, expected_str);
+}
+
+TEST_F(MemTableTest, CanConvertFromStringRepresentationWithTags) {
+  auto str{
+    "3\n"
+    "{00000000000000000001}{metric1}{tag1:value1} | 1\n"
+    "{00000000000000000002}{metric2}{tag2:value2} | 2\n"
+    "{00000000000000000003}{metric3}{tag3:value3} | 3\n"
+  };
+
+  Table table;
+  Table::fromString(str, table);
+
+  TimeSeriesKey key1{1, "metric1", {{"tag1", "value1"}}};
+  TimeSeriesKey key2{2, "metric2", {{"tag2", "value2"}}};
+  TimeSeriesKey key3{3, "metric3", {{"tag3", "value3"}}};
+
+  auto value1{table.get(key1)};
+  auto value2{table.get(key2)};
+  auto value3{table.get(key3)};
 
   EXPECT_EQ(value1, 1);
   EXPECT_EQ(value2, 2);
