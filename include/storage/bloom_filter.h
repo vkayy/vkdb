6b84918e6271c18ed1e3750f36b64a1255514065
vkayy <vkrice2475@gmail.com>
2024-12-12 20:48:14 +0000

    refactor: separate bloom filter into h/cpp as no longer templated

--- include/storage/bloom_filter.h
+++ include/storage/bloom_filter.h
@@ -4,6 +4,9 @@
 #include "utils/murmur_hash_3.h"
 #include "storage/time_series_key.h"
 #include "utils/random.h"
+#include <vector>
+#include <stdexcept>
+#include <cmath>
 
 class BloomFilter {
 public:
@@ -15,33 +18,7 @@ public:
 
   BloomFilter() = delete;
 
-  explicit BloomFilter(uint64_t expected_no_of_elems, double false_positive_rate) {
-    if (expected_no_of_elems == 0) {
-      throw std::invalid_argument{
-        "BloomFilter(): Expected elements must be greater than 0."
-      };
-    }
-
-    if (false_positive_rate <= MIN_FALSE_POSITIVE_RATE ||
-        false_positive_rate >= MAX_FALSE_POSITIVE_RATE) {
-      throw std::invalid_argument{
-        "BloomFilter(): False positive rate must be in the range (0, 1)."
-      };
-    }
-
-    auto log_false_positive_rate{std::log(false_positive_rate)};
-    auto log_2_squared{std::log(2) * std::log(2)};
-
-    auto no_of_bits{static_cast<size_type>(
-      -(expected_no_of_elems * log_false_positive_rate) / log_2_squared
-    )};
-    bits_.resize(no_of_bits);
-
-    auto no_of_hashes{static_cast<size_type>(
-      (no_of_bits / expected_no_of_elems) * std::log(2)
-    )};
-    initialise_seeds(no_of_hashes);
-  }
+  explicit BloomFilter(uint64_t expected_no_of_elems, double false_positive_rate);
 
   ~BloomFilter() = default;
 
@@ -51,37 +28,16 @@ public:
   BloomFilter(const BloomFilter&) = delete;
   BloomFilter& operator=(const BloomFilter&) = delete;
 
-  void insert(const key_type& key) noexcept {
-    for (size_type i{0}; i < seeds_.size(); ++i) {
-      bits_[hash(key, i)] = true;
-    }
-  }
+  void insert(const key_type& key) noexcept;
 
-  [[nodiscard]] bool mayContain(const key_type& key) const noexcept {
-    for (size_type i{0}; i < seeds_.size(); ++i) {
-      if (!bits_[hash(key, i)]) {
-        return false;
-      }
-    }
-    return true;
-  }
+  [[nodiscard]] bool mayContain(const key_type& key) const noexcept;
 
 private:
   using HashValue = uint32_t;
 
-  void initialise_seeds(size_type no_of_hashes) {
-    seeds_.resize(no_of_hashes);
-    for (size_type i{0}; i < no_of_hashes; ++i) {
-      seeds_[i] = random<size_type>();
-    }
-  }
+  void initialise_seeds(size_type no_of_hashes);
 
-  [[nodiscard]] HashValue hash(const key_type& key, size_type i) const noexcept {
-    HashValue hash_value{0};
-    auto std_hash_value{std::hash<std::string>{}(key.toString())};
-    MurmurHash3_x86_32(&std_hash_value, sizeof(std_hash_value), i, &hash_value);
-    return hash_value % bits_.size();
-  }
+  [[nodiscard]] HashValue hash(const key_type& key, size_type i) const noexcept;
 
   std::vector<size_type> seeds_;
   std::vector<bool> bits_;
---
+++ src/storage/bloom_filter.cpp
@@ -0,0 +1,58 @@
+#include "storage/bloom_filter.h"
+
+BloomFilter::BloomFilter(uint64_t expected_no_of_elems, double false_positive_rate) {
+  if (expected_no_of_elems == 0) {
+    throw std::invalid_argument{
+      "BloomFilter(): Expected elements must be greater than 0."
+    };
+  }
+
+  if (false_positive_rate <= MIN_FALSE_POSITIVE_RATE ||
+      false_positive_rate >= MAX_FALSE_POSITIVE_RATE) {
+    throw std::invalid_argument{
+      "BloomFilter(): False positive rate must be in the range (0, 1)."
+    };
+  }
+
+  auto log_false_positive_rate{std::log(false_positive_rate)};
+  auto log_2_squared{std::log(2) * std::log(2)};
+
+  auto no_of_bits{static_cast<size_type>(
+    -(expected_no_of_elems * log_false_positive_rate) / log_2_squared
+  )};
+  bits_.resize(no_of_bits);
+
+  auto no_of_hashes{static_cast<size_type>(
+    (no_of_bits / expected_no_of_elems) * std::log(2)
+  )};
+  initialise_seeds(no_of_hashes);
+}
+
+void BloomFilter::insert(const key_type& key) noexcept {
+  for (size_type i{0}; i < seeds_.size(); ++i) {
+    bits_[hash(key, i)] = true;
+  }
+}
+
+bool BloomFilter::mayContain(const key_type& key) const noexcept {
+  for (size_type i{0}; i < seeds_.size(); ++i) {
+    if (!bits_[hash(key, i)]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void BloomFilter::initialise_seeds(size_type no_of_hashes) {
+  seeds_.resize(no_of_hashes);
+  for (size_type i{0}; i < no_of_hashes; ++i) {
+    seeds_[i] = random<size_type>();
+  }
+}
+
+BloomFilter::HashValue BloomFilter::hash(const key_type& key, size_type i) const noexcept {
+  HashValue hash_value{0};
+  auto std_hash_value{std::hash<std::string>{}(key.toString())};
+  MurmurHash3_x86_32(&std_hash_value, sizeof(std_hash_value), i, &hash_value);
+  return hash_value % bits_.size();
+}
