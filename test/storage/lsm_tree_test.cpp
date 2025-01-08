df562f6779346872898b9c57cbe58b014fadbdd6
vkayy <vkrice2475@gmail.com>
2025-01-08 10:11:22 +0000

    test: fix typo in test parameter for lsm tree flushing

--- test/storage/lsm_tree_test.cpp
+++ test/storage/lsm_tree_test.cpp
@@ -257,7 +257,7 @@ TEST_F(LSMTreeTest, CanReplayWriteAheadLog) {
 }
 
 TEST_F(LSMTreeTest, CanPutWithFlushing) {
-  for (Timestamp i{0}; i < 100'000'000; ++i) {
+  for (Timestamp i{0}; i < 100'000; ++i) {
     TimeSeriesKey key{i, "metric", {}};
     lsm_tree_->put(key, i);
   }
